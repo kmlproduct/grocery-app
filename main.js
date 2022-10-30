9482e923151a590e9689d79f04ba96459d5a2c30
kmlproduct <kam.salaheddine@gmail.com>
2022-10-30 14:21:26 +0100

    add edit button

--- main.js
+++ main.js
@@ -3,109 +3,111 @@ var PareTable = document.getElementById('table-info');
 var inputName = document.getElementById('input-name');
 var inputPrice = document.getElementById('input-price');
 var myTable = document.querySelector('#table tbody');
-var myAmount = document.getElementById('total-price')
+var totalSpending = document.getElementById('total-price')
+var parentModal = document.getElementById('parent-modal')
 
-console.log(myTable)
 
 
+
+function getData() {
+    const storedItems = localStorage.getItem('localData');
+    return storedItems ? JSON.parse(storedItems) : [];
+}
 
 addInfo.addEventListener('click', function () {
 
     var groceryItem = {
-        name: `${inputName.value}`,
-        price: `${inputPrice.value}`,
+        name: inputName.value,
+        price: inputPrice.value,
         id: new Date().getTime(),
     }
 
     myTable.innerHTML += `<tr id="${groceryItem.id}">
-    <td class='name'>${groceryItem.name}  </td> 
+    <td class='name'> ${groceryItem.name}  </td> 
     <td class='price'> ${groceryItem.price}</td> 
-    <td class='delate'>delate</td> 
+    <td class='delete'>delete</td> 
+    <td class='edit'>edit</td>
     </tr>`
 
     // localStorage
 
 
-    var storedItems = localStorage.getItem('localData')
+    const groceryItems = getData();
 
-    const groceryItems = storedItems ? JSON.parse(storedItems) : [];
 
     groceryItems.push(groceryItem)
 
-    localStorage.setItem('localData', JSON.stringify(groceryItems))
+
 
     // localeStorage
-    let Amount = document.getElementsByClassName('price');
 
-    let count1 = []
+    const sum = groceryItems.reduce((a, b) => a + parseInt(b.price), 0)
+    totalSpending.innerText = sum.toFixed(2)
+    localStorage.setItem('localData', JSON.stringify(groceryItems))
+    let totalAmount = parseInt(totalSpending.innerText)
+    console.log(totalAmount)
 
-
-    for (var i = 0; i < Amount.length; i++) {
-        var count = JSON.parse(Amount[i].textContent)
-        count1.push(count)
-
+    if (totalAmount < 300) {
+        document.getElementById('alert').innerText = ``
+        totalSpending.style.background = '#00ff8c';
+        totalSpending.style.color = 'black';
+        document.getElementById('alert').style.background = 'none'
+    } else if (totalAmount >= 300) {
+        document.getElementById('alert').innerText = `you have to control your salary`
+        totalSpending.style.background = '#eb911e';
+        totalSpending.style.color = 'white';
+        document.getElementById('alert').style.background = 'white'
     }
-    let sum1 = count1.reduce((a, b) => a + b, 0)
-    myAmount.innerText = `${sum1}.00`
-
-    var delate = document.querySelectorAll('.delate');
+    var deleteBtns = document.querySelectorAll('.delete');
 
 
-    let totalAmount = JSON.parse(myAmount.innerText)
+
+    deleteBtns[deleteBtns.length - 1].onclick = function () {
 
 
-    delate[delate.length - 1].onclick = function () {
-        // delate[delate.length - 1]
-
-        let deleteLocalItem = JSON.parse(this.parentNode.getAttribute('id'))
+        let deleteLocalItem = parseInt(this.parentNode.getAttribute('id'))
         const removeIndex = groceryItems.findIndex(item => item.id === deleteLocalItem);
+        const groceryItem = groceryItems.find(item => item.id === deleteLocalItem);
+
         groceryItems.splice(removeIndex, 1);
         localStorage.setItem('localData', JSON.stringify(groceryItems))
+        totalSpending.textContent = parseInt(totalSpending.textContent) - groceryItem.price;
 
-
+        if (totalSpending.textContent < 300) {
+            console.log(totalAmount)
+            document.getElementById('alert').innerText = ``
+            totalSpending.style.background = '#00ff8c';
+            totalSpending.style.color = 'black';
+            document.getElementById('alert').style.background = 'none'
+        }
         this.parentNode.remove();
-        var x = this.parentNode.childNodes[3].textContent
-        x = JSON.parse(x);
-        myAmount.textContent = JSON.parse(myAmount.textContent) - x;
-
-
 
 
     }
-    if (totalAmount > 300) {
-
-        document.getElementById('alert').innerText = `you have to control your salary`
-        myAmount.style.background = '#eb911e';
-        myAmount.style.color = 'white';
-        document.getElementById('alert').style.background = 'white'
 
 
-    } else {
-        document.getElementById('alert').innerText = ``
-        myAmount.style.background = 'none';
-        document.getElementById('alert').style.background = 'none'
-    }
 })
 
 
 window.onload = function () {
 
-    var storedItems = localStorage.getItem('localData');
-    var groceryItems = storedItems ? JSON.parse(storedItems) : [];
+    var groceryItems = getData();
     groceryItems.forEach(element => {
         myTable.innerHTML +=
             `<tr id="${element.id}">
         <td class="name">${element.name}  </td> 
         <td class='price'>${element.price}  </td> 
-        <td class ="delate">Delate</td> 
+        <td class ="delete">delete</td> 
+        <td class ="edit" >edit</td> 
         </tr>`
     });
+
 
     // groceryItems = groceryItems.map(element => {
     //     return `<tr>
     //     <th>${element.name}  </th> 
     //     <th>${element.price}  </th> 
-    //     <th class="delate">delate</th> 
+    //     <th class="delete">delete</th> 
     //     </tr>`
     // });
 
@@ -119,13 +121,25 @@ window.onload = function () {
         count.push(count1)
     }
     let sum1 = count.reduce((a, b) => a + b, 0)
-    myAmount.textContent = `${sum1}.00`
+    totalSpending.textContent = sum1.toFixed(2)
+
+    if (sum1 >= 300) {
+        document.getElementById('alert').innerText = `you have to control your salary`
+        totalSpending.style.background = '#eb911e';
+        totalSpending.style.color = 'white';
+        document.getElementById('alert').style.background = 'white'
+    } else if (totalSpending.textContent < 300) {
+        document.getElementById('alert').innerText = ``
+        totalSpending.style.background = '#00ff8c';
+        totalSpending.style.color = 'black';
+        document.getElementById('alert').style.background = 'none'
+    }
     // calculate sum
 
-    let delate = document.getElementsByClassName('delate');
-    let totalAmount = JSON.parse(myAmount.innerText)
-    for (var i = 0; i < delate.length; i++) {
-        delate[i].onclick = function () {
+    let deleteBtns = document.getElementsByClassName('delete');
+
+    for (var i = 0; i < deleteBtns.length; i++) {
+        deleteBtns[i].onclick = function () {
 
             let deleteLocalItem = JSON.parse(this.parentNode.getAttribute('id'))
 
@@ -140,19 +154,53 @@ window.onload = function () {
             this.parentNode.remove()
             var x = this.parentNode.childNodes[3].textContent
             x = JSON.parse(x);
-            myAmount.textContent = JSON.parse(myAmount.textContent) - x;
+            totalSpending.textContent = JSON.parse(totalSpending.textContent) - x;
+            if (totalSpending.textContent < 300) {
+                document.getElementById('alert').innerText = ``
+                totalSpending.style.background = '#00ff8c';
+                totalSpending.style.color = 'black';
+                document.getElementById('alert').style.background = 'none'
+            }
+        }
+    }
+
+    let editBtn = document.getElementsByClassName('edit')
+    let editInputName = document.getElementById('edit-input-name')
+    let editInputPrice = document.getElementById('edit-input-price')
+    let saveInfo = document.getElementById('save-change')
+
+    for (var i = 0; i < editBtn.length; i++) {
+
+
+        editBtn[i].onclick = function () {
+            let editLocalItem = JSON.parse(this.parentNode.getAttribute('id'))
+
+            parentModal.style.display = 'block'
+            let groceryItem = groceryItems.find(item => item.id === editLocalItem);
+            editInputName.value = groceryItem.name
+            editInputPrice.value = groceryItem.price
+
+            saveInfo.onclick = function () {
+
+
+                groceryItem.name = editInputName.value
+                groceryItem.price = editInputPrice.value,
+
+                    //     groceryItems.push(groceryItem)
+
+                    localStorage.setItem('localData', JSON.stringify(groceryItems))
+
+                parentModal.style.display = 'none'
+
+            }
+
+
+
+
+
+
 
         }
 
-    }
-    if (totalAmount > 300) {
-        document.getElementById('alert').innerText = `you have to control your salary`
-        myAmount.style.background = '#eb911e';
-        myAmount.style.color = 'white';
-        document.getElementById('alert').style.background = 'white'
-    } else {
-        document.getElementById('alert').innerText = ``
-        myAmount.style.background = 'none';
-        document.getElementById('alert').style.background = 'none'
     }
 }
